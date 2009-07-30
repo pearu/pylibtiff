4faf3b3494978ffb4db44d345ea26706255f57c2
pearu.peterson <pearu.peterson@4a3e2f78-2e7b-11de-9b0f-33ff132f608d>
2009-07-30 14:15:33 +0000

    Added support to reading and writing int, float, and complex data types.

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -5,7 +5,10 @@ from numpy import *
 from libtiff import TIFF
 
 def test_write_read():
-    for itype in [uint8, uint16, uint32, uint32]:
+    for itype in [uint8, uint16, uint32, uint64, 
+                  int8, int16, int32, int64,
+                  float32, float64,
+                  complex64, complex128]:
 
         image = array([[1,2,3], [4,5,6]], itype)
         fn = mktemp('.tif')
