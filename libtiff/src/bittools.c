da65f5f43621a262be86dd672e74a12b49ec16e0
pearu.peterson <pearu.peterson@4a3e2f78-2e7b-11de-9b0f-33ff132f608d>
2010-05-14 11:58:32 +0000

    Cleaned up bittools, added more tests.

--- libtiff/src/bittools.c
+++ libtiff/src/bittools.c
@@ -6,30 +6,35 @@
 #define PyMODINIT_FUNC void
 #endif
 
-/* i/8 == i>>3 */
-#define BITS(bytes)  ((((npy_intp) (bytes)) << 3)
-#define BYTES(bits)  (((bits) == 0) ? 0 : ((((bits) - 1) >> 3) + 1))
-#define NBYTES(bits)  ((bits) >> 3)
-#define BITMASK(i,width)  (((char) 1) << ((i)%(width)))
-#define DATAPTR(data, i) ((char*)(data) + ((i)>>3))
-#define DATA(data, i) (*DATAPTR((data),(i)))
-#define GETBIT(value, i, width) ((value & BITMASK((i),width)) ? 1 : 0)
-#define ARRGETBIT(arr, i) ((DATA(PyArray_DATA(arr), (i)) & BITMASK((i),8)) ? 1 : 0)
+#define CHAR_BITS 8
+#define CHAR_BITS_EXP 3
 
-static PyObject *getbit(PyObject *self, PyObject *args)
+/* i/8 == i>>3 */
+#define BITS(bytes)  (((unsigned long)(bytes)) << CHAR_BITS_EXP)
+#define BYTES(bits)  (((bits) == 0) ? 0 : ((((bits) - 1) >> CHAR_BITS_EXP) + 1))
+#define NBYTES(bits)  ((bits) >> CHAR_BITS_EXP)
+#define BITMASK(i,width)  (((unsigned long) 1) << (((i))%(width)))
+#define DATAPTR(data, i) ((char*)(data) + ((i)>>CHAR_BITS_EXP))
+#define DATA(data, i) (*DATAPTR((data),(i)))
+#define GETBIT(value, i, width) ((value & BITMASK((i),(width))) ? 1 : 0)
+#define ARRGETBIT(arr, i) ((DATA(PyArray_DATA(arr), (i)) & BITMASK((i),CHAR_BITS)) ? 1 : 0)
+
+static PyObject *getbit(PyObject *self, PyObject *args, PyObject *kwds)
 {
   PyObject* arr = NULL;
   char bit = 0;
-  npy_uintp index = 0;
-
-  if (!PyArg_ParseTuple(args, "Ok", &arr, &index))
+  Py_ssize_t index = 0;
+  static char* kwlist[] = {"array", "index", NULL};
+  if (!PyArg_ParseTupleAndKeywords(args, kwds, "|On:getbit", 
+				   kwlist, &arr, &index))
     return NULL;
+
   if (!PyArray_Check(arr))
     {
       PyErr_SetString(PyExc_TypeError,"first argument must be array object");
       return NULL;
     }
-  if (NBYTES(index) >= PyArray_NBYTES(arr))
+  if (index >= BITS(PyArray_NBYTES(arr)))
     {
       PyErr_SetString(PyExc_IndexError,"bit index out of range");
       return NULL;
@@ -38,12 +43,14 @@ static PyObject *getbit(PyObject *self, PyObject *args)
   return Py_BuildValue("b",bit);
 }
 
-static PyObject *setbit(PyObject *self, PyObject *args)
+static PyObject *setbit(PyObject *self, PyObject *args, PyObject *kwds)
 {
   PyObject* arr = NULL;
   char bit = 0;
-  npy_uintp index = 0;
-  if (!PyArg_ParseTuple(args, "Okb", &arr, &index, &bit))
+  Py_ssize_t index = 0;
+  static char* kwlist[] = {"array", "index", "bit", NULL};
+  if (!PyArg_ParseTupleAndKeywords(args, kwds, "|Onb:setbit", 
+				   kwlist, &arr, &index, &bit))
     return NULL;
   if (!PyArray_Check(arr))
     {
@@ -56,84 +63,96 @@ static PyObject *setbit(PyObject *self, PyObject *args)
       return NULL;
     }
   if (bit)
-    DATA(PyArray_DATA(arr), index) |= BITMASK(index, 8);
+    DATA(PyArray_DATA(arr), index) |= BITMASK(index, CHAR_BITS);
   else
-    DATA(PyArray_DATA(arr), index) &= ~BITMASK(index, 8);
+    DATA(PyArray_DATA(arr), index) &= ~BITMASK(index, CHAR_BITS);
   Py_INCREF(Py_None);
   return Py_None;
 }
 
-static PyObject *getword(PyObject *self, PyObject *args)
+static PyObject *getword(PyObject *self, PyObject *args, PyObject *kwds)
 {
   PyObject* arr = NULL;
-  npy_uintp index = 0;
-  char width = 0, i;
-
-  arr = PyTuple_GET_ITEM(args, 0);
-  index = PyLong_AsUnsignedLongMask(PyTuple_GET_ITEM(args, 1));
-  width = PyLong_AsUnsignedLongMask(PyTuple_GET_ITEM(args, 2));
-  /*
-  if (!PyArg_ParseTuple(args, "Okb", &arr, &index, &width))
+  Py_ssize_t index = 0;
+  Py_ssize_t width = 0, i;
+  static char* kwlist[] = {"array", "index", "width", NULL};
+  if (!PyArg_ParseTupleAndKeywords(args, kwds, "|Onn:getword", 
+				   kwlist, &arr, &index, &width))
     return NULL;
   if (!PyArray_Check(arr))
     {
       PyErr_SetString(PyExc_TypeError,"first argument must be array object");
       return NULL;
     }
-  if (NBYTES(index+width-1) >= PyArray_NBYTES(arr))
+  if (((index+width-1) >= BITS(PyArray_NBYTES(arr))) || (width<0))
     {
       PyErr_SetString(PyExc_IndexError,"bit index out of range");
       return NULL;
     }
-  */
   
+  // fast code, at least 3x
   if (width<=32)
     {
-      npy_uint32 x = *((npy_uint64*)DATAPTR(PyArray_DATA(arr), index)) >> (index % 8);
-      return Py_BuildValue("I",x & (NPY_MAX_UINT32>>(32-width)));
+      npy_uint32 x = *((npy_uint64*)DATAPTR(PyArray_DATA(arr), index)) >> (index % CHAR_BITS);
+      return Py_BuildValue("k",x & (NPY_MAX_UINT32>>(32-width)));
     }
-  
   // generic code
   if (width<=64)
     {
       npy_uint64 word = 0;
-      for (i=index; i<index+width; ++i)
-	if (ARRGETBIT(arr, i))
-	  word |= BITMASK(i, width);
+      for (i=0; i<width; ++i)
+	if (ARRGETBIT(arr, index + i))
+	    word |= BITMASK(i, width);
 	else
-	  word &= ~BITMASK(i, width);
+	    word &= ~BITMASK(i, width);
+
       return Py_BuildValue("k",word);
     }
-  // todo: implement support for widths>64
+  PyErr_SetString(PyExc_ValueError,"bit width must not be larger than 64");
   return NULL;
 }
 
-static PyObject *setword(PyObject *self, PyObject *args)
+static PyObject *setword(PyObject *self, PyObject *args, PyObject *kwds)
 {
   PyObject* arr = NULL;
-  npy_uintp index = 0;
-  char width = 0, i;
-  npy_int32 value = 0;
-  arr = PyTuple_GET_ITEM(args, 0);
-  index = PyLong_AsUnsignedLongMask(PyTuple_GET_ITEM(args, 1));
-  width = PyLong_AsUnsignedLongMask(PyTuple_GET_ITEM(args, 2));
-  value = PyLong_AsUnsignedLongMask(PyTuple_GET_ITEM(args, 3));
+  Py_ssize_t index = 0;
+  Py_ssize_t width = 0, i, value_width=sizeof(npy_uint64)*CHAR_BITS;
+  npy_uint64 value = 0;
+  static char* kwlist[] = {"array", "index", "width", "value", NULL};
+  if (!PyArg_ParseTupleAndKeywords(args, kwds, "|Onnk:setword", 
+				   kwlist, &arr, &index, &width, &value))
+    return NULL;
+  if (!PyArray_Check(arr))
+    {
+      PyErr_SetString(PyExc_TypeError,"first argument must be array object");
+      return NULL;
+    }
+  if ((index+width-1) >= BITS(PyArray_NBYTES(arr)) || width<0)
+    {
+      PyErr_SetString(PyExc_IndexError,"bit index out of range");
+      return NULL;
+    }
+  if (width>64)
+    {
+      PyErr_SetString(PyExc_ValueError,"bit width must not be larger than 64");
+      return NULL;
+    }
 
   for (i=0; i<width; ++i)
-    if (GETBIT(value, i, 32))
-      DATA(PyArray_DATA(arr), index+i) |= BITMASK(index+i, 8);
+    if ((i<value_width) && (GETBIT(value, i, value_width)))
+      DATA(PyArray_DATA(arr), index+i) |= BITMASK(index+i, CHAR_BITS);
     else
-      DATA(PyArray_DATA(arr), index+i) &= ~BITMASK(index+i, 8);
+      DATA(PyArray_DATA(arr), index+i) &= ~BITMASK(index+i, CHAR_BITS);
 
   Py_INCREF(Py_None);
   return Py_None;
 }
 
 static PyMethodDef module_methods[] = {
-  {"getbit", getbit, METH_VARARGS, "Get bit value of an array at bit index."},
-  {"setbit", setbit, METH_VARARGS, "Set bit value of an array at bit index."},
-  {"getword", getword, METH_VARARGS, "getword(array, bitindex, wordwidth) - get word value from an array at bitindex with bitwidth."},
-  {"setword", setword, METH_VARARGS, "setword(array, bitindex, wordwidth, word) - set word value to an array at bitindex with bitwidth."},
+  {"getbit", (PyCFunction)getbit, METH_VARARGS|METH_KEYWORDS, "Get bit value of an array at bit index."},
+  {"setbit", (PyCFunction)setbit, METH_VARARGS|METH_KEYWORDS, "Set bit value of an array at bit index."},
+  {"getword", (PyCFunction)getword, METH_VARARGS|METH_KEYWORDS, "getword(array, bitindex, wordwidth) - get word value from an array at bitindex with bitwidth."},
+  {"setword", (PyCFunction)setword, METH_VARARGS|METH_KEYWORDS, "setword(array, bitindex, wordwidth, word) - set word value to an array at bitindex with bitwidth."},
   {NULL}  /* Sentinel */
 };
 
--- libtiff/test_bittools.py
+++ libtiff/test_bittools.py
@@ -2,6 +2,9 @@
 import numpy
 import bittools
 
+def tobinary(arr):
+    return ''.join([str(bittools.getbit (arr,i)) for i in range (arr.nbytes*8)])
+
 def test_setgetbit():
 
     # bit-wise copy
@@ -14,11 +17,29 @@ def test_setgetbit():
 
     print 'ok'
 
-def test_getword():
-    arr = numpy.array([63330], dtype=numpy.uint16)
-    print bittools.getword(arr, 1, arr.dtype.itemsize * 8-1)
+def test_setgetword():
+    for dtype in [numpy.ubyte, numpy.int32, numpy.float64]:
+        arr = numpy.array(range(-256,256), dtype=dtype)
+        arr2 = numpy.zeros(arr.shape, dtype=arr.dtype)
+        for i in range (arr.nbytes):
+            word = bittools.getword (arr,i*8,8)
+            bittools.setword (arr2,i*8,8,word)
+        assert (arr==arr2).all(),`arr,arr2`
     print 'ok'
+
+def test_wordbits ():
+    for width in range (1,64+1):
+        arr = numpy.array([131,17,235], dtype=numpy.int64)
+        arr2 = numpy.zeros((1+width//8), dtype=arr.dtype)
+        bstr = tobinary(arr)
+        word = bittools.getword(arr, 0, width)
+        if width > 7:
+            assert word==arr[0],`word, arr[0], bstr`
+        bittools.setword (arr2, 0, width, word)
+        assert bittools.getword (arr2, 0, width)==word
+        assert tobinary(arr2)[:width] == bstr[:width],`tobinary(arr2)[:width], bstr[:width]`
 
 if __name__=='__main__':
     test_setgetbit()
-    test_getword()
+    test_setgetword()
+    test_wordbits ()
