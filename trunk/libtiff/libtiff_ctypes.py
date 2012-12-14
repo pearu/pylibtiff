90bf5a0833cd000e249c515a9de8e30fb67c40d7
joe.jordan86@gmail.com <joe.jordan86@gmail.com@4a3e2f78-2e7b-11de-9b0f-33ff132f608d>
2012-12-14 13:51:39 +0000

    adding TIFF3D class to automatically handle the inverse of 3D monochrome numpy writes.

--- trunk/libtiff/libtiff_ctypes.py
+++ trunk/libtiff/libtiff_ctypes.py
@@ -999,6 +999,115 @@ class TIFF(ctypes.c_void_p):
                 break
         other.close ()
 
+class TIFF3D(TIFF):
+    """ subclass of TIFF for handling import of 3D (multi-directory) files.
+    
+    like TIFF, but TIFF3D.read_image() will attempt to restore a 3D numpy array
+    when given a multi-image TIFF file; performing the inverse of
+    
+    TIFF_instance.write(numpy.zeros((40, 200, 200)))
+    
+    like so:
+    
+    arr = TIFF3D_instance.read_image()
+    arr.shape # gives (40, 200, 200)
+    
+    if you tried this with a normal TIFF instance, you would get this:
+    
+    arr = TIFF_instance.read_image()
+    arr.shape # gives (200, 200)
+    
+    and you would have to loop over each image by hand with TIFF.iter_images().
+    """
+    @classmethod
+    def open(cls, filename, mode='r'):
+        """ just like TIFF.open, except returns a TIFF3D instance.
+        """
+        # monkey-patch the restype:
+        old_restype = libtiff.TIFFOpen.restype
+        libtiff.TIFFOpen.restype = TIFF3D
+
+        # actually call the library function:
+        tiff = libtiff.TIFFOpen(filename, mode)
+
+        # restore the old restype:
+        libtiff.TIFFOpen.restype = old_restype
+        if tiff.value is None:
+            raise TypeError ('Failed to open file '+`filename`)
+        return tiff
+
+    @debug
+    def read_image(self, verbose=False, as3d=True):
+        """ Read image from TIFF and return it as a numpy array.
+        
+        If as3d is passed True (default), will attempt to read multiple
+        directories, and restore as slices in a 3D array. ASSUMES that all
+        images in the tiff file have the same width, height, bits-per-sample,
+        compression, and so on. If you get a segfault, this is probably the
+        problem.
+        """
+        if not as3d:
+            return TIFF.read_image(self, verbose)
+
+        # Code is initially copy-paste from TIFF:
+        width = self.GetField('ImageWidth')
+        height = self.GetField('ImageLength')
+        bits = self.GetField('BitsPerSample')
+        sample_format = self.GetField('SampleFormat')
+        compression = self.GetField('Compression')
+
+        typ = self.get_numpy_type(bits, sample_format)
+
+        if typ is None:
+            if bits==1:
+                typ = np.uint8
+                itemsize = 1
+            elif bits==4:
+                typ = np.uint32
+                itemsize = 4
+            else:
+                raise NotImplementedError (`bits`)
+        else:
+            itemsize = bits/8
+
+
+        # in order to allocate the numpy array, we must count the directories:
+        # code borrowed from TIFF.iter_images():
+        depth = 0
+        while True:
+            depth += 1
+            if self.LastDirectory():
+                break
+            self.ReadDirectory()
+        self.SetDirectory(0)
+
+        # we proceed assuming all directories have the same properties from above.
+        layer_size = width * height * itemsize
+        total_size = layer_size * depth
+        arr = np.zeros((depth, height, width), typ)
+
+        if compression == COMPRESSION_NONE:
+            ReadStrip = self.ReadRawStrip
+        else:
+            ReadStrip = self.ReadEncodedStrip
+
+        layer = 0
+        while True:
+            pos = 0
+            elem = None
+            for strip in range (self.NumberOfStrips()):
+                if elem is None:
+                    elem = ReadStrip(strip, arr.ctypes.data + layer * layer_size + pos, layer_size)
+                elif elem:
+                    elem = ReadStrip(strip, arr.ctypes.data + layer * layer_size + pos, min(layer_size - pos, elem))
+                pos += elem
+            if self.LastDirectory():
+                break
+            self.ReadDirectory()
+            layer += 1
+        self.SetDirectory(0)
+        return arr
+
 import struct
 import numpy
 class CZ_LSMInfo:
