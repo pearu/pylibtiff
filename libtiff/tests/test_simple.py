9f0fb5b0bbee5e329102c2c73b7612527201994a
Joshua Taillon <jat255@gmail.com>
2016-04-04 14:31:43 -0400

    pep8 corrections

--- libtiff/tests/test_simple.py
+++ libtiff/tests/test_simple.py
@@ -1,45 +1,45 @@
-
 import os
 from tempfile import mktemp
 from numpy import *
 from libtiff import TIFF
 
+
 def test_write_read():
-    for itype in [uint8, uint16, uint32, uint64, 
+    for itype in [uint8, uint16, uint32, uint64,
                   int8, int16, int32, int64,
                   float32, float64,
                   complex64, complex128]:
-
-        image = array([[1,2,3], [4,5,6]], itype)
+        image = array([[1, 2, 3], [4, 5, 6]], itype)
         fn = mktemp('.tif')
-        tif = TIFF.open(fn,'w')
+        tif = TIFF.open(fn, 'w')
         tif.write_image(image)
         tif.close()
-        
-        tif = TIFF.open(fn,'r')
+
+        tif = TIFF.open(fn, 'r')
         image2 = tif.read_image()
         tif.close()
         os.remove(fn)
 
-        assert image.dtype==image2.dtype
-        assert (image==image2).all()
+        assert image.dtype == image2.dtype
+        assert (image == image2).all()
+
 
 def test_slicing():
     shape = (16, 16)
     image = random.randint(255, size=shape)
-    
+
     for i in range(shape[0]):
         for j in range(shape[1]):
-            image1 = image[:i+1,:j+1]
+            image1 = image[:i + 1, :j + 1]
             fn = mktemp('.tif')
-            tif = TIFF.open(fn,'w')
+            tif = TIFF.open(fn, 'w')
             tif.write_image(image1)
             tif.close()
-        
-            tif = TIFF.open(fn,'r')
+
+            tif = TIFF.open(fn, 'r')
             image2 = tif.read_image()
             tif.close()
-            
-            assert (image1==image2).all(),repr((i,j))
+
+            assert (image1 == image2).all(), repr((i, j))
 
             os.remove(fn)
--- libtiff/tests/test_tiff_array.py
+++ libtiff/tests/test_tiff_array.py
@@ -6,6 +6,7 @@ from numpy import *
 from libtiff import TIFF
 from libtiff import TIFFfile, TIFFimage
 
+
 def test_simple_slicing():
     for planar_config in [1,2]:
         for compression in [None, 'lzw']:
@@ -13,33 +14,35 @@ def test_simple_slicing():
                           int8, int16, int32, int64,
                           float32, float64,
                           complex64, complex128]:
-                image = random.randint(0, 100, size=(10,6,7)).astype (itype)
+                image = random.randint(0, 100, size=(10,6,7)).astype(itype)
                 fn = mktemp('.tif')
 
                 if 0:
-                    if planar_config==2:
+                    if planar_config == 2:
                         continue
-                    tif = TIFF.open(fn,'w')
+                    tif = TIFF.open(fn, 'w')
                     tif.write_image(image, compression=compression)
                     tif.close()
                 else:
                     tif = TIFFimage(image)
-                    tif.write_file(fn, compression=compression, planar_config=planar_config)
+                    tif.write_file(fn,
+                                   compression=compression,
+                                   planar_config=planar_config)
                     del tif
 
                 tif = TIFFfile(fn)
                 arr = tif.get_tiff_array()
                 data = arr[:]
-                assert len(data)==len (image), repr(len(data))
-                assert image.dtype==data.dtype, repr((image.dtype, data[0].dtype))
-                assert (image==data).all()
-                assert arr.shape==image.shape
+                assert len(data) == len(image), repr(len(data))
+                assert image.dtype == data.dtype, repr((image.dtype,
+                                                       data[0].dtype))
+                assert (image == data).all()
+                assert arr.shape == image.shape
 
-                indices = [0, slice(None), slice (0,2), slice (0,5,2)]
-                for i0 in indices[:1]:
-                    for i1 in indices:
-                        for i2 in indices:
-                            sl = (i0, i1, i2)
-                            assert (arr[sl]==image[sl]).all(),repr(sl)
-                #os.remove(fn)
+                _indices = [0, slice(None), slice(0, 2), slice(0, 5, 2)]
+                for _i0 in _indices[:1]:
+                    for i1 in _indices:
+                        for i2 in _indices:
+                            sl = (_i0, i1, i2)
+                            assert (arr[sl] == image[sl]).all(),repr(sl)
                 atexit.register(os.remove, fn)
