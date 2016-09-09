1cc78a9b9c359905003d006388ca5b189030d2a7
Kris Kvilekval <kris@cs.ucsb.edu>
2016-09-09 09:38:40 -0700

    2.7 compat fix, allow use of wheel

--- setup.py
+++ setup.py
@@ -105,8 +105,8 @@ PyLibTiff? is a Python package that provides the following modules:
    tiff - a numpy.memmap view of tiff files.
 ''',
           platforms=["All"],
-          # packages = ['libtiff'],
-          # package_dir = {'libtiff': 'libtiff'},
+          packages = ['libtiff'],
+          package_dir = {'libtiff': 'libtiff'},
           configuration=configuration,
           include_dirs = [ np.get_include()], 
           ext_modules=[bittools_mod, tif_lzw_mod], requires=['numpy']
