735be7009f2ce673bf9301b0e1cea15724a0bbe2
David Hoese <david.hoese@ssec.wisc.edu>
2022-12-12 13:18:23 -0600

    Fix flake8 issues in tests

--- libtiff/tests/test_libtiff_ctypes.py
+++ libtiff/tests/test_libtiff_ctypes.py
@@ -1,6 +1,4 @@
-import os
 import pytest
-from tempfile import mktemp
 import numpy as np
 from libtiff import TIFFimage
 
@@ -16,4 +14,3 @@ def test_issue69(tmp_path):
     del tif
     tif = lt.TIFF3D.open(fn)
     tif.close()
-
