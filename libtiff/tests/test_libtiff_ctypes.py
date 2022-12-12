4e688b38d9388406196f0591a3f510929a00f334
David Hoese <david.hoese@ssec.wisc.edu>
2022-12-12 13:08:22 -0600

    Replace usage of atexit with tmp_path in test_libtiff_ctypes.py

--- libtiff/tests/test_libtiff_ctypes.py
+++ libtiff/tests/test_libtiff_ctypes.py
@@ -1,20 +1,19 @@
 import os
 import pytest
 from tempfile import mktemp
-import atexit
 import numpy as np
 from libtiff import TIFFimage
 
 lt = pytest.importorskip('libtiff.libtiff_ctypes')
 
 
-def test_issue69():
+def test_issue69(tmp_path):
     itype = np.uint32
     image = np.array([[[1, 2, 3], [4, 5, 6]]], itype)
-    fn = mktemp('issue69.tif')
+    fn = str(tmp_path / "issue69.tif")
     tif = TIFFimage(image)
     tif.write_file(fn)
     del tif
     tif = lt.TIFF3D.open(fn)
     tif.close()
-    atexit.register(os.remove, fn)
+
